b6d047cf2e4d374c2098bce0ae2ae753e1795d7b
Stepan Bylkov <scroogemcfawk@gmail.com>
2024-11-22 18:57:46 +0700

    feat: add signal traps fix: piping on exit

--- start.sh
+++ start.sh
@@ -1,17 +1,41 @@
 #!/bin/bash
 
+stop() {
+  # stop application
+  echo "Terminating application with PID: $PID"
+  bash "./stop.sh"
+  exit 0
+}
+
 # set current directory to script's parent directory
-cd "$(dirname "$0")" || (echo "Failed to set workdir" | exit 1)
+cd "$(dirname "$0")" || { echo "Failed to set workdir"; exit 1; }
+
+# check if process is running
+if [[ -f ".pid" ]]; then
+    echo "Process with $(cat .pid) is already running"
+    exit 2
+fi
 
 # locate jar file
 JAR="$(find . -name "samurai-1*.jar")"
 
 # check if jar file exists
-if [ ! -f "$JAR" ]; then
-    echo "Failed to locate jar file" | exit 2
+if [[ ! -f "$JAR" ]]; then
+  echo "Failed to locate jar file"
+  exit 3
 fi
 
-# run application
+# set argument if it is not provided
+if [[ ! $1 ]]; then
+  echo "Config name is not provided, using default"
+  set -- "./config/config.json"
+fi
+
+# set signal handling
+trap "echo Ignoring the signal" HUP QUIT
+trap stop INT TERM
+
+# run application in background detached from terminal
 nohup java -jar "$JAR" "$1" &
 
 PID="$!"
@@ -21,3 +45,5 @@ echo "$PID" > ".pid"
 
 # echo application PID
 echo "Application started with PID: $PID"
+
+wait $PID
--- stop.sh
+++ stop.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 # set current directory to script's parent directory
-cd "$(dirname "$0")" || (echo "Failed to set workdir" | exit 1)
+cd "$(dirname "$0")" || { echo "Failed to set workdir"; exit 1; }
 
-# check if jar file exists
-if [ ! -f ".pid" ]; then
-    echo ".pid file not found" | exit 2
+# check if .pid file exists
+if [[ ! -f ".pid" ]]; then
+    echo ".pid file not found"
+    exit 2
 fi
 
 # read pid form file
 PID="$(cat ".pid")"
 
 # stop application
-kill "$PID"
+kill -TERM "$PID"
 
 rm ".pid"
